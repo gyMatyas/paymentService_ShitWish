8695aee7239951e92f35c66f6c7cb6e0d1c85b6c
György Mátyás <gy.matyas97@gmail.com>
2018-03-28 18:49:54 +0200

    payment logic nearly completed

--- src/main/java/com/shitwish/paymentService/PaymentServiceApp/Controller/PaymentServiceController.java
+++ src/main/java/com/shitwish/paymentService/PaymentServiceApp/Controller/PaymentServiceController.java
@@ -3,13 +3,17 @@ package com.shitwish.paymentService.PaymentServiceApp.Controller;
 import com.google.gson.Gson;
 import com.shitwish.paymentService.PaymentServiceApp.Model.Payment;
 import com.shitwish.paymentService.PaymentServiceApp.Service.PaymentService;
+import org.apache.http.client.fluent.Request;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -28,74 +32,88 @@ public class PaymentServiceController {
     }
 
     @RequestMapping(value = "/pay", method = RequestMethod.POST)
-    public String initiatePayment(@RequestParam("userId") String json) {
-        int userId = Integer.valueOf(json);
+    public String initiatePayment(@RequestBody Map<String, String> data) {
+        boolean succeeded;
 
-        // TODO: Here comes the request for UserService (getting the buyer)
+        JSONObject response = new JSONObject();
+        int userId = Integer.valueOf(data.get("userId"));
+
+        // Request for UserService (getting the buyer)
 
         String buyerResponse = restTemplate.getForObject("https://shitwish-user.herokuapp.com/user/" + userId, String.class);
         JSONObject buyerJSON = new JSONObject(buyerResponse);
         int buyerBalance = (int) buyerJSON.get("balance");
 
-        // TODO: Here comes the request for OrderService (getting the order with products) with fake status
+        // Request for OrderService (getting the order with products)
 
-        //String orderResponse = restTemplate.getForObject("orderservice/getForUser/" + userId, String.class);
-        JSONObject orderJSON = new JSONObject("{ 'orders': [ { 'field1' : 'hello' }, { 'field1' : 'world' } ]} ");
-        JSONArray orders = (JSONArray) orderJSON.get("orders");
-                //TODO: Getting the seller's id from products and price
-                for (int i = 0; i < orders.length(); i++){
-                    System.out.println(orders.getJSONObject(i).get("field1"));
+        String orderResponse = restTemplate.getForObject("https://shitwish-order-service.herokuapp.com/get-active-order/" + userId, String.class);
+        System.out.println(orderResponse);
+        JSONObject order = new JSONObject(orderResponse);
+        // Getting the seller's id from products and price
+        if (order.has("productIds")) {
+            double totalPrice = 0;
+            List<Map<String, String>> sellers = new LinkedList<>();
+            JSONArray productIds = order.getJSONArray("productIds");
+
+            for (Object productId : productIds) {
+                totalPrice = service.getSellersData(totalPrice, sellers, (int) productId);
+            }
+            succeeded = totalPrice <= buyerBalance;
+
+            // Process of payment
+
+            if (succeeded) {
+                for (Map<String, String> seller : sellers) {
+                    try {
+                        Payment payment = service.payUser(userId, seller);
+                        service.savePayment(payment);
+                    } catch (IOException e) {
+                        service.responseError(response, "Payment has encountered an error!");
+                        return String.valueOf(response);
+                    }
+                }
+                try {
+                    service.withdrawUser(userId, (int) totalPrice);
+                } catch (IOException e) {
+                    service.responseError(response, "Withdrawing money encountered an error!");
+                    return String.valueOf(response);
                 }
 
-        // TODO: Here comes process of payment
+                for (Object productId : productIds) {
+                    try {
+                        Request.Post("http://shitwish-product.herokuapp.com/products/sell-product?id=" + (int) productId )
+                                .execute();
+                        System.out.println("Changed the status for product : " + (int) productId);
+                    } catch (IOException e) {
+                        service.responseError(response, "Changing the product status encountered an error!");
+                        return String.valueOf(response);
+                    }
+                }
 
-        boolean succeeded = true;
-        // TODO: POST REQUESTS TO User Service to change balance
+                service.responseSuccess(response);
+                return String.valueOf(response);
+            }
+            // TODO: Here comes the status change request for OrderService
+            // TODO: POST REQUEST to Order Service
 
-
-        // TODO: Here comes the status change request for OrderService
-        // TODO: POST REQUEST to Order Service
-
-        // TODO: Here comes the request for Product service (change quantity)
-        // TODO: POST REQUEST TO Product Service
-
-
-        JSONObject response = new JSONObject();
-
-        if (succeeded) {
-            Payment payment = new Payment( (long) userId, (long) userId, buyerBalance);
-            service.savePayment(payment);
-            response.put("success", true);
-            response.put("message", "Payment successful!");
-        } else {
-            response.put("success", false);
-            response.put("message", "Insufficient wallet balance.");
+            // TODO: Here comes the request for Product service (change quantity)
+            // TODO: POST REQUEST TO Product Service
         }
+        service.responseError(response, "Insufficient wallet balance.");
         return String.valueOf(response);
     }
 
-
     @RequestMapping(value = "/payment/{id}", method = RequestMethod.GET)
     public String getPayment(@PathVariable("id") String id) {
-        Map<String, Object> responseObject = new HashMap<>();
-
-        /** DIS IS HOW YOU POST SOMETHING TY JAVA //
-        try {
-            Request.Post("http://localhost:8080/pay")
-                    .bodyForm(Form.form().add("userId", "1").build())
-                    .execute();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-         **/
+        Map<String, Object> responseMap = new HashMap<>();
         try {
             long userId = Long.parseLong(id);
-            responseObject.put("bought", service.getPaymentsByBuyer(userId));
-            responseObject.put("sold", service.getPaymentsBySeller(userId));
+            responseMap.put("bought", service.getPaymentsByBuyer(userId));
+            responseMap.put("sold", service.getPaymentsBySeller(userId));
         } catch (NumberFormatException e) {
-            responseObject.put("message", "Wrong user ID.");
+            responseMap.put("message", "Wrong user ID.");
         }
-        return String.valueOf(gson.toJson(responseObject));
+        return String.valueOf(gson.toJson(responseMap));
     }
 
 }
--- src/main/java/com/shitwish/paymentService/PaymentServiceApp/Service/PaymentService.java
+++ src/main/java/com/shitwish/paymentService/PaymentServiceApp/Service/PaymentService.java
@@ -1,14 +1,32 @@
 package com.shitwish.paymentService.PaymentServiceApp.Service;
 
+import com.google.gson.Gson;
 import com.shitwish.paymentService.PaymentServiceApp.Model.Payment;
 import com.shitwish.paymentService.PaymentServiceApp.Repository.PaymentRepository;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+
+    private Gson gson = new Gson();
+
+    private final RestTemplate restTemplate = new RestTemplate();
 
     @Autowired
     PaymentRepository repository;
@@ -22,4 +40,57 @@ public class PaymentService {
     }
 
     public void savePayment(Payment payment) { repository.save(payment); }
+
+    public Payment payUser(long userId, Map<String, String> seller) throws IOException {
+        String postUrl = "https://shitwish-user.herokuapp.com/user/" + seller.get("userId");
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(postUrl);
+        StringEntity postingString = new StringEntity(gson.toJson(seller));
+        post.setEntity(postingString);
+        post.setHeader("Content-type", "application/json");
+        HttpResponse response = httpClient.execute(post);
+        System.out.println("User " + seller.get("userId") + " paid for " + seller.get("value"));
+        System.out.println(EntityUtils.toString(response.getEntity()));
+        return new Payment(userId, Long.valueOf(seller.get("userId")), Integer.valueOf(seller.get("value")));
+    }
+
+    public double getSellersData(double totalPrice, List<Map<String, String>> sellers, int productId) {
+        String product = restTemplate.getForObject("http://shitwish-product.herokuapp.com/products/" + productId, String.class);
+        Map<String, String> sellerInfo = new HashMap<>();
+        JSONObject productObject = new JSONObject(product);
+        int currentPrice = (int) productObject.get("price");
+        int currentUserId = (int) productObject.get("userId");
+        sellerInfo.put("userId", String.valueOf(currentUserId));
+        sellerInfo.put("value", String.valueOf(currentPrice));
+        sellers.add(sellerInfo);
+        totalPrice += currentPrice;
+        System.out.println("Total price : " + (int) totalPrice);
+        System.out.println("Seller Info: " + sellerInfo);
+        return totalPrice;
+    }
+
+    public void withdrawUser(long userId, int totalPrice) throws IOException {
+        Map<String, String> buyer = new HashMap<>();
+        buyer.put("userId", String.valueOf(userId));
+        buyer.put("value", String.valueOf(totalPrice * -1));
+        String postUrl = "https://shitwish-user.herokuapp.com/user/" + buyer.get("userId");
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(postUrl);
+        StringEntity postingString = new StringEntity(gson.toJson(buyer));
+        post.setEntity(postingString);
+        post.setHeader("Content-type", "application/json");
+        HttpResponse response = httpClient.execute(post);
+        System.out.println("User " + buyer.get("userId") + " withdraw for " + buyer.get("value"));
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+
+    public void responseSuccess(JSONObject response) {
+        response.put("success", true);
+        response.put("message", "Payment successful!");
+    }
+
+    public void responseError(JSONObject response, String errorString) {
+        response.put("success", false);
+        response.put("message", errorString);
+    }
 }
